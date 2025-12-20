fefc2ed400261b195bed5c3c684ab5745b1c7ab4
mcducx <kirill.belle@gmail.com>
2025-12-21 01:37:16 +0200

    Remove unused touch and display configuration from config.h and display.h

--- src/config.h
+++ src/config.h
@@ -7,15 +7,5 @@
 #define SD_MOSI  11
 #define SD_CS    10
 
-// ==================== Touch Configuration ====================
-#define TOUCH_GT911_SDA 19
-#define TOUCH_GT911_SCL 20
-#define TOUCH_GT911_INT -1
-#define TOUCH_GT911_RST 38
-
-// ==================== Display Configuration ====================
-#define TFT_BL   2
-#define DISP_WIDTH  800
-#define DISP_HEIGHT 480
 
 #endif // CONFIG_H
--- src/display.h
+++ src/display.h
@@ -6,14 +6,10 @@
 #include <SD.h>
 #include <SPI.h>
 
-// ==================== SD Card Configuration ====================
-#define SD_CS    10
-#define SD_SCK   12 
-#define SD_MISO  13
-#define SD_MOSI  11
-
 // ==================== Display & Touch Configuration ====================
 #define TFT_BL 2
+#define DISP_WIDTH  800
+#define DISP_HEIGHT 480
 #define TOUCH_GT911_SCL 20
 #define TOUCH_GT911_SDA 19
 #define TOUCH_GT911_INT -1
